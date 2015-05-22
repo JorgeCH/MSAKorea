da58257f76ebb0cd05466e95fa541a8b82ba88ac
JorgeCH <el.chicharo.magico@gmail.com>
2015-05-22 13:21:02 -0500

    User model with password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150521061115) do
+ActiveRecord::Schema.define(version: 20150522172127) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "name"
@@ -101,6 +101,14 @@ ActiveRecord::Schema.define(version: 20150521061115) do
     t.string   "link"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
