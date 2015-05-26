9a637a0d650bb35f073cedd678a85a1cdc4ac1a1
JorgeCH <el.chicharo.magico@gmail.com>
2015-05-26 14:18:03 -0500

    profile pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]  
+
+  def index
+    @users = User.order(:email)
+  end
+
   def new
     @user = User.new
   end
@@ -13,9 +19,39 @@ class UsersController < ApplicationController
       render :action => "new"
     end
   end
+
+  def show
+   
+  end
+
+  def edit
+  end
+
+  def update
+    if params[:user][:password].blank?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
+    
+    if @user.update(user_params)
+      flash[:notice] = "El Usuario se ha modificado."
+      redirect_to @user
+    else
+      flash[:alert] = "El Usuario no ha sido modificado."
+      render action: "edit"
+    end
+  end
+
+  def destroy
+
+  end
     
   private
     def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+    def set_user
+       @user = User.find(params[:id])
+    end 
 end
